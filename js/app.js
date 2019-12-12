d90d979c950e57cd2b96d7ce3fd9f1a66973ea7f
dannevernguyens <danguyenuw@gmail.com>
2019-12-11 20:19:13 -0800

    built constructor function, reorged code, built checkuserdata function

--- js/app.js
+++ js/app.js
@@ -1,22 +1,61 @@
 'use strict';
 
-function displayRounds(event) {
-    event.preventDefault();
-    hide(nameScreen);
-    show(roundsScreen);
+//GLOBAL VARIABLES
 
-}
-
-function hide(elem){
-    elem.style.display = 'none';
-}
-
-function show(elem){
-    elem.style.display = 'block';
-}
-
+//DOM MANIPULATING VARS
 var nameForm = document.getElementById('name-form');
 var nameScreen = document.getElementById('name-screen');
 var roundsScreen = document.getElementById('rounds-screen');
 
+//EVENT LISTENERS
 nameForm.addEventListener('submit', displayRounds);
+nameForm.addEventListener('submit', checkUserData);
+
+//PLAYER OBJECT
+var playerObject = {};
+
+//PLAYER CONSTRUCTOR
+function Player (playerName) {
+  this.playerName = playerName;
+  this.roundsChosen = 0;
+  this.roundsWon = 0;
+  this.roundsLost = 0;
+  this.bestOf3Wins = 0;
+  this.bestOf5Wins = 0;
+  this.bestOf7Wins = 0;
+  this.totalGamesPlayed = 0;
+  this.totalGamesWon = 0;
+  this.settings = [];
+}
+
+//FUNCTION TO DISPLAY ROUNDS
+function displayRounds(event) {
+  event.preventDefault();
+  hide(nameScreen);
+  show(roundsScreen);
+}
+
+//function to hide
+function hide(elem){
+  elem.style.display = 'none';
+}
+
+//function to show
+function show(elem){
+  elem.style.display = 'block';
+}
+
+
+
+//FUNCTION TO CHECK FOR USER DATA
+function checkUserData(event) {
+  event.preventDefault();
+  var playerName = event.target.nameInput.value;
+  if (localStorage.getItem(playerName)) {
+    var getName = JSON.parse(localStorage.getItem(playerName));
+    playerObject = getName;
+  } else {
+    playerObject = new Player(playerName);
+  } console.log('player object is ',playerObject);
+}
+//FUNCTION TO CREATE CONSTRUCTOR OBJECT
