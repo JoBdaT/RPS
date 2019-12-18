5ee7d2def412528cf1e6831a8ae2c04497396d86
dannevernguyens <danguyenuw@gmail.com>
2019-12-17 18:48:49 -0800

    added tie counter to UI

--- js/app.js
+++ js/app.js
@@ -53,6 +53,7 @@ function Player (playerName) {
   this.countRound = 0;
   this.countWin = 0;
   this.countLoss = 0;
+  this.countTie = 0;
 }
 
 // =================================================== //
@@ -65,11 +66,7 @@ function popNames() {
 
 function popUI() {
   roundUICount.textContent = `Round ${playerObject.countRound} of ${playerObject.roundsChosen}`;
-  winLossUICount.textContent = `${playerObject.countWin} Wins & ${playerObject.countLoss} Losses`;
-}
-
-function clearUI() {
-
+  winLossUICount.textContent = `${playerObject.countWin} Wins & ${playerObject.countLoss} Losses & ${playerObject.countTie} Ties`;
 }
 
 // vv ====== UI ====== vv //
@@ -133,6 +130,7 @@ function roundCounter (roundsChosen) {
     playerObject.countWin = 0;
     playerObject.countLoss = 0;
     playerObject.countRound = 0;
+    playerObject.countTie = 0;
     break;
   case 5:
     playerObject.roundsWon = 3;
@@ -140,6 +138,7 @@ function roundCounter (roundsChosen) {
     playerObject.countWin = 0;
     playerObject.countLoss = 0;
     playerObject.countRound = 0;
+    playerObject.countTie = 0;
     break;
   case 7:
     playerObject.roundsWon = 4;
@@ -147,6 +146,7 @@ function roundCounter (roundsChosen) {
     playerObject.countWin = 0;
     playerObject.countLoss = 0;
     playerObject.countRound = 0;
+    playerObject.countTie = 0;
     break;
   }
 }
@@ -243,6 +243,7 @@ function declareWinner (userWeapon, cpuWeapon, winner) {
   if (winner === 'tie') {
     // animate tie
     testVictory.textContent = 'tie';
+    playerObject.countTie++;
   } else if (userWeapon === winner) {
     // animate userWeapon victory
     testVictory.textContent = 'User Wins';
