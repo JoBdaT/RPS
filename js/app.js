3341f9859f470684c7662e3c7e02485159b62396
Clayton Jones <claytonjonesLMP@gmail.com>
2019-12-17 18:46:49 -0800

    repositioned new image animations to be centered

--- js/app.js
+++ js/app.js
@@ -310,44 +310,44 @@ function draw(userWeapon, cpuWeapon) {
   cpuPaper.scaleY = 0.15;
   var userScissors = new createjs.Bitmap('../images/scissors2.png');
   userScissors.scaleX = 0.15;
-  userScissors.scaleY = 0.15;
+  userScissors.scaleY = 0.20;
   var cpuScissors = new createjs.Bitmap('../images/scissors2.png');
   cpuScissors.scaleX = 0.15;
-  cpuScissors.scaleY = 0.15;
+  cpuScissors.scaleY = 0.20;
 
 
   if (userWeapon === 'rock') {
     userRock.x = 50;
-    userRock.y = 200;
+    userRock.y = 150;
     stage.addChild(userRock);
 
     if (cpuWeapon === 'rock') {
-      cpuRock.x = 450;
-      cpuRock.y = 200;
+      cpuRock.x = 375;
+      cpuRock.y = 150;
       stage.addChild(cpuRock);
 
       // userRock animation against cpuRock
       createjs.Tween.get(userRock, { loop: false })
-        .to({ x: 200 }, 1000, createjs.Ease.getPowInOut(4))
-        .to({ x: 190 }, 50, createjs.Ease.getPowInOut(4))
-        .to({ x: 200 }, 50, createjs.Ease.getPowInOut(4))
-        .to({ x: 190 }, 50, createjs.Ease.getPowInOut(4))
-        .to({ x: 200 }, 50, createjs.Ease.getPowInOut(4))
-        .to({ x: 190 }, 50, createjs.Ease.getPowInOut(4))
-        .to({ x: 200 }, 50, createjs.Ease.getPowInOut(4))
+        .to({ x: 160 }, 1000, createjs.Ease.getPowInOut(4))
+        .to({ x: 150 }, 50, createjs.Ease.getPowInOut(4))
+        .to({ x: 160 }, 50, createjs.Ease.getPowInOut(4))
+        .to({ x: 150 }, 50, createjs.Ease.getPowInOut(4))
+        .to({ x: 160 }, 50, createjs.Ease.getPowInOut(4))
+        .to({ x: 150 }, 50, createjs.Ease.getPowInOut(4))
+        .to({ x: 160 }, 50, createjs.Ease.getPowInOut(4))
         .to({ alpha: 0.5 }, 400)
         .to({ alpha: 1 }, 400)
         .to({ alpha: 0 }, 400);
 
       // cpuRock animation against userRock
       createjs.Tween.get(cpuRock, { loop: false })
-        .to({ x: 300 }, 1000, createjs.Ease.getPowInOut(4))
-        .to({ x: 310 }, 50, createjs.Ease.getPowInOut(4))
-        .to({ x: 300 }, 50, createjs.Ease.getPowInOut(4))
-        .to({ x: 310 }, 50, createjs.Ease.getPowInOut(4))
-        .to({ x: 300 }, 50, createjs.Ease.getPowInOut(4))
-        .to({ x: 310 }, 50, createjs.Ease.getPowInOut(4))
-        .to({ x: 300 }, 50, createjs.Ease.getPowInOut(4))
+        .to({ x: 265 }, 1000, createjs.Ease.getPowInOut(4))
+        .to({ x: 275 }, 50, createjs.Ease.getPowInOut(4))
+        .to({ x: 265 }, 50, createjs.Ease.getPowInOut(4))
+        .to({ x: 275 }, 50, createjs.Ease.getPowInOut(4))
+        .to({ x: 265 }, 50, createjs.Ease.getPowInOut(4))
+        .to({ x: 275 }, 50, createjs.Ease.getPowInOut(4))
+        .to({ x: 265 }, 50, createjs.Ease.getPowInOut(4))
         .to({ alpha: 0.5 }, 400)
         .to({ alpha: 1 }, 400)
         .to({ alpha: 0 }, 400);
@@ -357,16 +357,16 @@ function draw(userWeapon, cpuWeapon) {
     }
 
     if (cpuWeapon === 'paper') {
-      cpuPaper.x = 450;
+      cpuPaper.x = 400;
       cpuPaper.y = 140;
       stage.addChild(cpuPaper);
 
       createjs.Tween.get(userRock, { loop: false })
-        .to({x: 200 }, 1000, createjs.Ease.getPowInOut(4))
-        .to({ x: 190 }, 50, createjs.Ease.getPowInOut(4))
-        .to({ x: 200 }, 50, createjs.Ease.getPowInOut(4))
-        .to({ x: 190 }, 50, createjs.Ease.getPowInOut(4))
-        .to({ x: 200 }, 50, createjs.Ease.getPowInOut(4))
+        .to({x: 160 }, 1000, createjs.Ease.getPowInOut(4))
+        .to({ x: 150 }, 50, createjs.Ease.getPowInOut(4))
+        .to({ x: 160 }, 50, createjs.Ease.getPowInOut(4))
+        .to({ x: 150 }, 50, createjs.Ease.getPowInOut(4))
+        .to({ x: 160 }, 50, createjs.Ease.getPowInOut(4))
         .to({x: -50, y: -50}, 300, createjs.Ease.getPowInOut(4))
         .to({ alpha: 0 }, 200);
 
@@ -377,7 +377,7 @@ function draw(userWeapon, cpuWeapon) {
         .to({ x: 260 }, 50, createjs.Ease.getPowInOut(4))
         .to({ x: 250 }, 50, createjs.Ease.getPowInOut(4))
         .to({ x: 260 }, 50, createjs.Ease.getPowInOut(4))
-        .to({ x: 200 }, 50, createjs.Ease.getPowInOut(4))
+        .to({ x: 210 }, 50, createjs.Ease.getPowInOut(4))
         .to({ alpha: 0.5 }, 400)
         .to({ alpha: 1 }, 400)
         .to({ alpha: 0 }, 400);
@@ -387,28 +387,28 @@ function draw(userWeapon, cpuWeapon) {
     }
 
     if (cpuWeapon === 'scissors') {
-      cpuScissors.x = 450;
-      cpuScissors.y = 200;
+      cpuScissors.x = 400;
+      cpuScissors.y = 130;
       stage.addChild(cpuScissors);
 
       createjs.Tween.get(userRock, { loop: false })
-        .to({ x: 200 }, 1000, createjs.Ease.getPowInOut(4))
-        .to({ x: 190 }, 50, createjs.Ease.getPowInOut(4))
-        .to({ x: 200 }, 50, createjs.Ease.getPowInOut(4))
-        .to({ x: 190 }, 50, createjs.Ease.getPowInOut(4))
-        .to({ x: 200 }, 50, createjs.Ease.getPowInOut(4))
-        .to({ x: 190 }, 50, createjs.Ease.getPowInOut(4))
-        .to({ x: 250 }, 50, createjs.Ease.getPowInOut(4))
+        .to({ x: 160 }, 1000, createjs.Ease.getPowInOut(4))
+        .to({ x: 150 }, 50, createjs.Ease.getPowInOut(4))
+        .to({ x: 160 }, 50, createjs.Ease.getPowInOut(4))
+        .to({ x: 150 }, 50, createjs.Ease.getPowInOut(4))
+        .to({ x: 160 }, 50, createjs.Ease.getPowInOut(4))
+        .to({ x: 150 }, 50, createjs.Ease.getPowInOut(4))
+        .to({ x: 220 }, 50, createjs.Ease.getPowInOut(4))
         .to({ alpha: 0.5 }, 400)
         .to({ alpha: 1 }, 400)
         .to({ alpha: 0 }, 400);
 
       createjs.Tween.get(cpuScissors, { loop: false })
-        .to({ x: 310 }, 1000, createjs.Ease.getPowInOut(4))
-        .to({ x: 320 }, 50, createjs.Ease.getPowInOut(4))
-        .to({ x: 310 }, 50, createjs.Ease.getPowInOut(4))
-        .to({ x: 320 }, 50, createjs.Ease.getPowInOut(4))
-        .to({ x: 310 }, 50, createjs.Ease.getPowInOut(4))
+        .to({ x: 260 }, 1000, createjs.Ease.getPowInOut(4))
+        .to({ x: 270 }, 50, createjs.Ease.getPowInOut(4))
+        .to({ x: 260 }, 50, createjs.Ease.getPowInOut(4))
+        .to({ x: 270 }, 50, createjs.Ease.getPowInOut(4))
+        .to({ x: 260 }, 50, createjs.Ease.getPowInOut(4))
         .to({ x: 500, y: -50}, 400, createjs.Ease.getPowInOut(4))
         .to({ alpha: 0 }, 400);
 
@@ -425,18 +425,18 @@ function draw(userWeapon, cpuWeapon) {
     stage.addChild(userPaper);
 
     if (cpuWeapon === 'paper') {
-      cpuPaper.x = 450;
+      cpuPaper.x = 400;
       cpuPaper.y = 140;
       stage.addChild(cpuPaper);
 
       createjs.Tween.get(userPaper, { loop: false })
-        .to({ x: 150 }, 1000, createjs.Ease.getPowInOut(4))
-        .to({ x: 140 }, 50, createjs.Ease.getPowInOut(4))
-        .to({ x: 150 }, 50, createjs.Ease.getPowInOut(4))
-        .to({ x: 140 }, 50, createjs.Ease.getPowInOut(4))
-        .to({ x: 150 }, 50, createjs.Ease.getPowInOut(4))
-        .to({ x: 140 }, 50, createjs.Ease.getPowInOut(4))
-        .to({ x: 150 }, 50, createjs.Ease.getPowInOut(4))
+        .to({ x: 175 }, 1000, createjs.Ease.getPowInOut(4))
+        .to({ x: 165 }, 50, createjs.Ease.getPowInOut(4))
+        .to({ x: 175 }, 50, createjs.Ease.getPowInOut(4))
+        .to({ x: 165 }, 50, createjs.Ease.getPowInOut(4))
+        .to({ x: 175 }, 50, createjs.Ease.getPowInOut(4))
+        .to({ x: 165 }, 50, createjs.Ease.getPowInOut(4))
+        .to({ x: 175 }, 50, createjs.Ease.getPowInOut(4))
         .to({ alpha: 0.5 }, 400)
         .to({ alpha: 1 }, 400)
         .to({ alpha: 0 }, 400);
@@ -455,60 +455,60 @@ function draw(userWeapon, cpuWeapon) {
     }
 
     if (cpuWeapon === 'rock') {
-      cpuRock.x = 450;
-      cpuRock.y = 200;
+      cpuRock.x = 400;
+      cpuRock.y = 150;
       stage.addChild(cpuRock);
 
       createjs.Tween.get(userPaper, { loop: false })
-        .to({ x: 150 }, 1000, createjs.Ease.getPowInOut(4))
-        .to({ x: 140 }, 50, createjs.Ease.getPowInOut(4))
-        .to({ x: 150 }, 50, createjs.Ease.getPowInOut(4))
-        .to({ x: 140 }, 50, createjs.Ease.getPowInOut(4))
-        .to({ x: 150 }, 50, createjs.Ease.getPowInOut(4))
-        .to({ x: 140 }, 50, createjs.Ease.getPowInOut(4))
+        .to({ x: 175 }, 1000, createjs.Ease.getPowInOut(4))
+        .to({ x: 165 }, 50, createjs.Ease.getPowInOut(4))
+        .to({ x: 175 }, 50, createjs.Ease.getPowInOut(4))
+        .to({ x: 165 }, 50, createjs.Ease.getPowInOut(4))
+        .to({ x: 175 }, 50, createjs.Ease.getPowInOut(4))
+        .to({ x: 165 }, 50, createjs.Ease.getPowInOut(4))
         .to({ x: 250 }, 50, createjs.Ease.getPowInOut(4))
-        .to({ x: 200 }, 50, createjs.Ease.getPowInOut(4))
+        .to({ x: 210 }, 50, createjs.Ease.getPowInOut(4))
         .to({ alpha: 0.5 }, 400)
         .to({ alpha: 1 }, 400)
         .to({ alpha: 0 }, 400);
 
       createjs.Tween.get(cpuRock, { loop: false })
-        .to({ x: 300 }, 1000, createjs.Ease.getPowInOut(4))
-        .to({ x: 310 }, 50, createjs.Ease.getPowInOut(4))
-        .to({ x: 300 }, 50, createjs.Ease.getPowInOut(4))
-        .to({ x: 310 }, 50, createjs.Ease.getPowInOut(4))
-        .to({ x: 300 }, 50, createjs.Ease.getPowInOut(4))
-        .to({ x: 310 }, 50, createjs.Ease.getPowInOut(4))
+        .to({ x: 275 }, 1000, createjs.Ease.getPowInOut(4))
+        .to({ x: 285 }, 50, createjs.Ease.getPowInOut(4))
+        .to({ x: 275 }, 50, createjs.Ease.getPowInOut(4))
+        .to({ x: 285 }, 50, createjs.Ease.getPowInOut(4))
+        .to({ x: 275 }, 50, createjs.Ease.getPowInOut(4))
+        .to({ x: 285 }, 50, createjs.Ease.getPowInOut(4))
         // .to({ x: 300 }, 50, createjs.Ease.getPowInOut(4))
         .to({ x: 600, y: -50}, 100, createjs.Ease.getPowInOut(4))
         .to({ alpha: 0 }, 100);
     }
 
     if (cpuWeapon === 'scissors') {
-      cpuScissors.x = 450;
-      cpuScissors.y = 200;
+      cpuScissors.x = 400;
+      cpuScissors.y = 130;
       stage.addChild(cpuScissors);
 
       createjs.Tween.get(userPaper, { loop: false })
-        .to({ x: 150 }, 1000, createjs.Ease.getPowInOut(4)) // meet
-        .to({ x: 140 }, 50, createjs.Ease.getPowInOut(4)) // 1
-        .to({ x: 150 }, 50, createjs.Ease.getPowInOut(4)) // 2 swing
-        .to({ x: 140 }, 50, createjs.Ease.getPowInOut(4)) // 3
-        .to({ x: 150 }, 50, createjs.Ease.getPowInOut(4)) // 4 swing
-        .to({ x: 140 }, 50, createjs.Ease.getPowInOut(4)) // 5
+        .to({ x: 175 }, 1000, createjs.Ease.getPowInOut(4)) // meet
+        .to({ x: 165 }, 50, createjs.Ease.getPowInOut(4)) // 1
+        .to({ x: 175 }, 50, createjs.Ease.getPowInOut(4)) // 2 swing
+        .to({ x: 165 }, 50, createjs.Ease.getPowInOut(4)) // 3
+        .to({ x: 175 }, 50, createjs.Ease.getPowInOut(4)) // 4 swing
+        .to({ x: 165 }, 50, createjs.Ease.getPowInOut(4)) // 5
         // .to({ x: 150 }, 50, createjs.Ease.getPowInOut(4)) // 6 swing
         .to({ x: -50, y: -50 }, 50, createjs.Ease.getPowInOut(4)) //
         .to({ alpha: 0 }, 400);
 
       createjs.Tween.get(cpuScissors, { loop: false })
-        .to({ x: 300 }, 1000, createjs.Ease.getPowInOut(4)) // meet
-        .to({ x: 310 }, 50, createjs.Ease.getPowInOut(4)) // 1
-        .to({ x: 300 }, 50, createjs.Ease.getPowInOut(4)) // 2 swing
-        .to({ x: 310 }, 50, createjs.Ease.getPowInOut(4)) // 3
-        .to({ x: 300 }, 50, createjs.Ease.getPowInOut(4)) // 4 swing
-        .to({ x: 310 }, 50, createjs.Ease.getPowInOut(4)) // 5
-        .to({ x: 200 }, 50, createjs.Ease.getPowInOut(4)) // 6 // hit away
-        .to({ x: 250 }, 50, createjs.Ease.getPowInOut(4))
+        .to({ x: 275 }, 1000, createjs.Ease.getPowInOut(4)) // meet
+        .to({ x: 285 }, 50, createjs.Ease.getPowInOut(4)) // 1
+        .to({ x: 275 }, 50, createjs.Ease.getPowInOut(4)) // 2 swing
+        .to({ x: 285 }, 50, createjs.Ease.getPowInOut(4)) // 3
+        .to({ x: 275 }, 50, createjs.Ease.getPowInOut(4)) // 4 swing
+        .to({ x: 285 }, 50, createjs.Ease.getPowInOut(4)) // 5
+        .to({ x: 275 }, 50, createjs.Ease.getPowInOut(4)) // 6 // hit away
+        .to({ x: 220 }, 50, createjs.Ease.getPowInOut(4))
         .to({ alpha: 0 }, 400);
 
     }
@@ -519,30 +519,30 @@ function draw(userWeapon, cpuWeapon) {
   }
   if (userWeapon === 'scissors') {
     userScissors.x = 50;
-    userScissors.y = 200;
+    userScissors.y = 130;
     stage.addChild(userScissors);
 
     if (cpuWeapon === 'scissors') {
-      cpuScissors.x = 450;
-      cpuScissors.y = 200;
+      cpuScissors.x = 400;
+      cpuScissors.y = 130;
       stage.addChild(cpuScissors);
 
       createjs.Tween.get(userScissors, { loop: false })
-        .to({ x: 210 }, 1000, createjs.Ease.getPowInOut(4))
-        .to({ x: 200 }, 50, createjs.Ease.getPowInOut(4))
-        .to({ x: 210 }, 50, createjs.Ease.getPowInOut(4))
-        .to({ x: 200 }, 50, createjs.Ease.getPowInOut(4))
-        .to({ x: 210 }, 50, createjs.Ease.getPowInOut(4))
+        .to({ x: 165 }, 1000, createjs.Ease.getPowInOut(4))
+        .to({ x: 155 }, 50, createjs.Ease.getPowInOut(4))
+        .to({ x: 165 }, 50, createjs.Ease.getPowInOut(4))
+        .to({ x: 155 }, 50, createjs.Ease.getPowInOut(4))
+        .to({ x: 165 }, 50, createjs.Ease.getPowInOut(4))
         .to({ alpha: 0.5 }, 400)
         .to({ alpha: 1 }, 400)
         .to({ alpha: 0 }, 400);
 
       createjs.Tween.get(cpuScissors, { loop: false })
-        .to({ x: 310 }, 1000, createjs.Ease.getPowInOut(4))
-        .to({ x: 320 }, 50, createjs.Ease.getPowInOut(4))
-        .to({ x: 310 }, 50, createjs.Ease.getPowInOut(4))
-        .to({ x: 320 }, 50, createjs.Ease.getPowInOut(4))
-        .to({ x: 310 }, 50, createjs.Ease.getPowInOut(4))
+        .to({ x: 265 }, 1000, createjs.Ease.getPowInOut(4))
+        .to({ x: 275 }, 50, createjs.Ease.getPowInOut(4))
+        .to({ x: 265 }, 50, createjs.Ease.getPowInOut(4))
+        .to({ x: 275 }, 50, createjs.Ease.getPowInOut(4))
+        .to({ x: 265 }, 50, createjs.Ease.getPowInOut(4))
         .to({ alpha: 0.5 }, 400)
         .to({ alpha: 1 }, 400)
         .to({ alpha: 0 }, 400);
@@ -553,28 +553,28 @@ function draw(userWeapon, cpuWeapon) {
     }
 
     if (cpuWeapon === 'rock') {
-      cpuRock.x = 450;
-      cpuRock.y = 200;
+      cpuRock.x = 400;
+      cpuRock.y = 140;
       stage.addChild(cpuRock);
 
       createjs.Tween.get(userScissors, { loop: false })
-        .to({ x: 210 }, 1000, createjs.Ease.getPowInOut(4))
-        .to({ x: 200 }, 50, createjs.Ease.getPowInOut(4))
-        .to({ x: 210 }, 50, createjs.Ease.getPowInOut(4))
-        .to({ x: 200 }, 50, createjs.Ease.getPowInOut(4))
-        .to({ x: 210 }, 50, createjs.Ease.getPowInOut(4))
+        .to({ x: 175 }, 1000, createjs.Ease.getPowInOut(4))
+        .to({ x: 165 }, 50, createjs.Ease.getPowInOut(4))
+        .to({ x: 175 }, 50, createjs.Ease.getPowInOut(4))
+        .to({ x: 165 }, 50, createjs.Ease.getPowInOut(4))
+        .to({ x: 175 }, 50, createjs.Ease.getPowInOut(4))
         .to({ x: -50, y: -50 }, 400, createjs.Ease.getPowInOut(4))
         // .to({ x: 210 }, 50, createjs.Ease.getPowInOut(4))
         .to({ alpha: 0 }, 100);
 
       createjs.Tween.get(cpuRock, { loop: false })
-        .to({ x: 300 }, 1000, createjs.Ease.getPowInOut(4))
-        .to({ x: 310 }, 50, createjs.Ease.getPowInOut(4))
-        .to({ x: 300 }, 50, createjs.Ease.getPowInOut(4))
-        .to({ x: 310 }, 50, createjs.Ease.getPowInOut(4))
-        .to({ x: 300 }, 50, createjs.Ease.getPowInOut(4))
+        .to({ x: 255 }, 1000, createjs.Ease.getPowInOut(4))
+        .to({ x: 265 }, 50, createjs.Ease.getPowInOut(4))
+        .to({ x: 255 }, 50, createjs.Ease.getPowInOut(4))
+        .to({ x: 265 }, 50, createjs.Ease.getPowInOut(4))
+        .to({ x: 255 }, 50, createjs.Ease.getPowInOut(4))
+        .to({ x: 180 }, 50, createjs.Ease.getPowInOut(4))
         .to({ x: 200 }, 50, createjs.Ease.getPowInOut(4))
-        .to({ x: 250 }, 50, createjs.Ease.getPowInOut(4))
         .to({ alpha: 0.5 }, 400)
         .to({ alpha: 1 }, 400)
         .to({ alpha: 0 }, 400);
@@ -584,30 +584,30 @@ function draw(userWeapon, cpuWeapon) {
     }
 
     if (cpuWeapon === 'paper') {
-      cpuPaper.x = 450;
+      cpuPaper.x = 400;
       cpuPaper.y = 140;
       stage.addChild(cpuPaper);
 
       createjs.Tween.get(userScissors, { loop: false })
-        .to({ x: 210 }, 1000, createjs.Ease.getPowInOut(4)) // meet
-        .to({ x: 200 }, 50, createjs.Ease.getPowInOut(4))
-        .to({ x: 210 }, 50, createjs.Ease.getPowInOut(4)) // hit
-        .to({ x: 200 }, 50, createjs.Ease.getPowInOut(4))
-        .to({ x: 210 }, 50, createjs.Ease.getPowInOut(4)) // hit
-        .to({ x: 200 }, 50, createjs.Ease.getPowInOut(4))
-        .to({ x: 300 }, 50, createjs.Ease.getPowInOut(4)) // win
-        .to({ x: 250 }, 50, createjs.Ease.getPowInOut(4))
+        .to({ x: 195 }, 1000, createjs.Ease.getPowInOut(4)) // meet
+        .to({ x: 185 }, 50, createjs.Ease.getPowInOut(4))
+        .to({ x: 195 }, 50, createjs.Ease.getPowInOut(4)) // hit
+        .to({ x: 185 }, 50, createjs.Ease.getPowInOut(4))
+        .to({ x: 195 }, 50, createjs.Ease.getPowInOut(4)) // hit
+        .to({ x: 185 }, 50, createjs.Ease.getPowInOut(4))
+        .to({ x: 250 }, 50, createjs.Ease.getPowInOut(4)) // win
+        .to({ x: 210 }, 50, createjs.Ease.getPowInOut(4))
         .to({ alpha: 0.5 }, 400)
         .to({ alpha: 1 }, 400)
         .to({ alpha: 0 }, 400);
 
       createjs.Tween.get(cpuPaper, { loop: false })
-        .to({ x: 250 }, 1000, createjs.Ease.getPowInOut(4)) // meet
-        .to({ x: 260 }, 50, createjs.Ease.getPowInOut(4))
-        .to({ x: 250 }, 50, createjs.Ease.getPowInOut(4)) // hit
-        .to({ x: 260 }, 50, createjs.Ease.getPowInOut(4))
-        .to({ x: 250 }, 50, createjs.Ease.getPowInOut(4)) // hit
-        .to({ x: 260 }, 50, createjs.Ease.getPowInOut(4))
+        .to({ x: 260 }, 1000, createjs.Ease.getPowInOut(4)) // meet
+        .to({ x: 270 }, 50, createjs.Ease.getPowInOut(4))
+        .to({ x: 260 }, 50, createjs.Ease.getPowInOut(4)) // hit
+        .to({ x: 270 }, 50, createjs.Ease.getPowInOut(4))
+        .to({ x: 260 }, 50, createjs.Ease.getPowInOut(4)) // hit
+        .to({ x: 270 }, 50, createjs.Ease.getPowInOut(4))
         // .to({ x: 250 }, 50, createjs.Ease.getPowInOut(4))
         // .to({ x: 260 }, 50, createjs.Ease.getPowInOut(4))
         .to({ x: 600, y: -50 }, 400, createjs.Ease.getPowInOut(4)) // fly away
